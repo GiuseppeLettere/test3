4e5d9b97f6dbd5600713dd2d7b1aae17383bf6bf
GiuseppeLettere <giuseppe.lettere@gmail.com>
2021-06-30 16:06:31 +0200

    secondo

---
+++ TcHmiProject6/FunctionJS1.js
@@ -0,0 +1,14 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="../Packages/Beckhoff.TwinCAT.HMI.Framework.12.746.3/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject6;
+        (function (TcHmiProject6) {
+            function FunctionJS1(par1) {
+            }
+            TcHmiProject6.FunctionJS1 = FunctionJS1;
+        })(TcHmiProject6 = Functions.TcHmiProject6 || (Functions.TcHmiProject6 = {}));
+        Functions.registerFunctionEx('FunctionJS1', 'TcHmi.Functions.TcHmiProject6', TcHmiProject6.FunctionJS1);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
